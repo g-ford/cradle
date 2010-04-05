16d8c1c806abe0a4eab9e2f9facfc9130c5e7ea8
Geoff Ford <g_ford@hotmail.com>
2010-04-05 14:08:41 +1000

    Added a Program emitter

--- code/src/Lbach/Emitter.hs
+++ code/src/Lbach/Emitter.hs
@@ -1,11 +1,14 @@
 module Lbach.Emitter
-    (module Lbach.Emitter.Control, emit)
+    (module Lbach.Emitter.Control, emit, emitP)
 where
 
 import Lbach.Grammar.Basics
 import Lbach.Emitter.Core
 import Lbach.Emitter.Expressions
 import Lbach.Emitter.Control
+
+emitP :: Program -> String
+emitP (Program b) = emitBlock b
 
 -- Turns an expression into the equvilent assembly
 emit :: Assign -> String
