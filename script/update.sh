2425254c4bb14bf76173431be4c661e45c48fff5
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-04-08 08:35:16 -0300

    Update update.sh

--- script/update.sh
+++ script/update.sh
@@ -15,7 +15,7 @@ cd $BS_HOME
 git checkout 3.0.0-wip
 git pull origin 3.0.0-wip
 cp -rf less/* $UP_HOME/_assets/bootstrap/
-cp -rf js/bootstrap-{transition,collapse}.js $UP_HOME/_assets/bootstrapjs/
+cp -f js/bootstrap-{transition,collapse}.js $UP_HOME/_assets/bootstrapjs/
 
 echo "updating font awesome..."
 cd $FA_HOME
