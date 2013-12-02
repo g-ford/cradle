9cadb07dcf34eff0a0409b051cbf3361dcba4288
Geoff Ford <g_ford@hotmail.com>
2013-12-02 22:24:03 +1000

    002 Converted to using types

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,21 @@
 import Data.Char
 
+data Expression = 
+  Num Char 
+  | Add Expression Expression
+  | Sub Expression Expression
+  deriving (Show) 
+
 expected x = error $ x ++ " expected"
 
 term x
-  | isDigit x = [x]
-  | otherwise = expected "Digit" 
+  | isDigit x = Num x
+  | otherwise = expected "Digit"
 
 addOperation x
-  | x == '+' = "+"
-  | x == '-' = "-"
+  | x == '+' = Add
+  | x == '-' = Sub
   | otherwise = expected "AddOp"
 
 expression (x:[]) = term x
-expression (x:y:zs) = (term x) ++ (addOperation y) ++ (expression zs)
-
-
+expression (x:y:zs) = (addOperation y) (expression [x]) (expression zs)
