a8a2e8290a2eca0bd19733adb68eb6f7b3b64159
Geoff Ford <g_ford@hotmail.com>
2013-12-12 17:05:43 +1000

    Assignment using combinators

--- src/Main.hs
+++ src/Main.hs
@@ -6,40 +6,84 @@ data Expression =
   | Sub Expression Expression
   deriving (Show) 
 
+data Assign = Assign Char Expression 
+	deriving Show
+
+type Parser a = String -> Maybe (a, String)
+
+
 expected x = error $ x ++ " expected"
 
-term x
-  | isDigit x = Num x
-  | otherwise = expected "Digit"
 
-addOperation x
-  | x == '+' = Add
-  | x == '-' = Sub
-  | otherwise = expected "AddOp"
+parse :: String -> Assign
+parse s = Assign id expr
+    where (id, expr) = case assign s of 
+            Nothing -> error "Invalid assignment"
+            Just ((a, b), _) -> (a, Num b)
 
 
-expression (x:[]) = term x
-expression (a:b:c:d:ds) = (addOperation d) (expression [a,b,c]) (expression ds)
-expression (x:y:zs) = (addOperation y) (expression [x]) (expression zs)
+
+char :: Parser Char
+char [] = Nothing
+char (x:xs) = Just(x, xs)
+
+digit :: Parser Char
+digit = char <=> isDigit
+
+space :: Parser Char
+space = char <=> isSpace    
+
+letter :: Parser Char
+letter = char <=> isAlpha
+
+alphanum :: Parser Char
+alphanum = digit <|> letter
+
+literal :: Char -> Parser Char
+literal c = char <=> (==c)
+
+assign :: Parser (Char, Char)
+assign = letter <+-> literal '=' <+> digit
 
 
--- Emitter Funtions
+-- Given a parser and a predicate return the parser only if it satisfies the predicate.
+infix 7 <=> 
+(<=>) :: Parser a -> (a -> Bool) -> Parser a 
+(parser <=> predicate) input =
+    case parser input of 
+        Nothing       -> Nothing 
+        Just(a,rest)  -> if (predicate a) then Just(a,rest) else Nothing
 
-emit expr = case expr of
-	Num x -> [x]
-	Add x y -> emit x ++ " + " ++ emit y
-	Sub x y -> emit x ++ " - " ++ emit y
+infixl 6 <+>
+(<+>) :: Parser a -> Parser b -> Parser (a, b)
+(m <+> n) cs = case m cs of
+    Nothing -> Nothing
+    Just (a, cs') -> case n cs' of
+        Nothing -> Nothing
+        Just (b, cs2) -> Just((a, b), cs2)
 
-emitLn s = "\t" ++ s ++ "\n"
+-- Sequence operator that discards the second result 
+infixl 6 <+-> 
+(<+-> ) :: Parser a -> Parser b -> Parser a
+(m <+->  n) cs = case m cs of
+    Nothing -> Nothing
+    Just (a, cs') -> case n cs' of
+        Nothing -> Nothing
+        Just (b, cs2) -> Just(a, cs2)
 
-popEbx = emitLn "POP ebx"
-pushEax = emitLn "PUSH eax"
-add = popEbx ++ emitLn "ADD eax, ebx"
-sub = popEbx ++ emitLn "SUB eax, ebx" ++ emitLn "NEG eax"
+-- Sequence operator that discards the first result       
+infixl 6 <-+> 
+(<-+> ) :: Parser a -> Parser b -> Parser b
+(m <-+>  n) cs = case m cs of
+    Nothing -> Nothing
+    Just (a, cs') -> case n cs' of
+        Nothing -> Nothing
+        Just (b, cs2) -> Just(b, cs2) 
 
-emitAsm expr = case expr of 
-	Num a   -> emitLn ("MOV eax, " ++ [a])
-	Add a b -> emitAsm b ++  pushEax ++ emitAsm a ++ add
-	Sub a b -> emitAsm b ++  pushEax ++ emitAsm a ++ sub
-
-parseAndEmit = emitAsm . expression
+-- Combine two parsers using a 'or' type operation        
+infixl 3 <|>
+(<|>) :: Parser a -> Parser a -> Parser a 
+(parserA <|> parserB) input = 
+	case parserA input of 
+    	Nothing -> parserB input 
+    	result -> result
