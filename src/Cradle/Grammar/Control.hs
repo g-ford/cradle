f2b5ba5854edd3ce7a75d8b4cfee3eaea5f43968
Geoff Ford <g_ford@hotmail.com>
2014-01-09 17:00:39 +1000

    Changed endif to end

--- src/Cradle/Grammar/Control.hs
+++ src/Cradle/Grammar/Control.hs
@@ -7,8 +7,10 @@ import Cradle.Grammar.Expressions
 
 data Program = Program Block deriving (Show)
 type Block = [Statement]
-data Statement = Statement Assign 
+data Statement = 
+	Statement Assign 
   | Branch Condition Block
+  | Branch2 Condition Block Block
   deriving (Show)
 
 type Condition = String
@@ -21,11 +23,16 @@ block = iterS statement
 
 statement :: Parser Statement
 statement = assign >>> Statement
+  <|> ifelse
   <|> ifthen 
 
 ifthen :: Parser Statement		
-ifthen = accept "if" <-+> condition <+> block <+-> accept "endif" >>> buildBranch
+ifthen = accept "if" <-+> condition <+> block <+-> accept "end" >>> buildBranch
     where buildBranch (c, b) = Branch c b
+
+ifelse :: Parser Statement		
+ifelse = accept "if" <-+> condition <+> block <+-> accept "else" <+> block <+-> accept "end" >>> buildBranch
+    where buildBranch ((c, b1), b2) = Branch2 c b1 b2
 
 condition = tempPlaceholder
 
